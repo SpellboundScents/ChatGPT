1cba8d407592e94903fa7c7cc13d7b87030730b5
chirv <inbox@spellboundscentswax.com>
2025-09-03 16:23:30 -0400

    modified:   casks/chatgpt.rb

--- casks/chatgpt.rb
+++ casks/chatgpt.rb
@@ -1,5 +1,5 @@
 cask "chatgpt" do
-   version "1.0.0"
+   version "1.1.0"
    sha256 "afe420f2a7297671389aae0080d67a9ed0bf69793d63d9f7f459e9d6082235dc"
 
   url "https://github.com/SpellboundScents/ChatGPT/releases/download/v#{version}/ChatGPT_#{version}_x64.dmg"
